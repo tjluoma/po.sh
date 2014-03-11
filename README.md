a4ffaa0959c136d19ff2549930f85cf9bd5be023
TJ Luoma <luomat@gmail.com>
2014-03-10 21:00:44 -0400

    Updated README and uploaded first draft of script

--- README.md
+++ README.md
@@ -1,4 +1,62 @@
-po.sh
-=====
+po.sh: **[P]**ush**[O]**ver **[SH]**ell script
+====
 
-A Zsh shell client for https://pushover.net
+## *What is Pushover?*
+
+[Pushover][] is a web service to send messages from a computer to an iOS (or Android) device.
+
+   [Pushover]: https://pushover.net/
+
+Previously I was using [Prowl][], but Pushover has a richer feature set and looks nicer, which is what gave me the idea to call my script *[posh][]*.
+
+   [Prowl]: http://prowlapp.com/
+   [posh]: https://www.google.com/search?q=+define+posh
+
+### *What is `po.sh`?*
+
+`posh` is a simple shell script that I wrote which uses the [Pushover REST API](https://pushover.net/api)  to easily send messages to myself from other shell scripts.
+
+For example, if I have a `cron` or `launchd` job that runs every night or every week, and I want to know that it was successful, I can just add this to the bottom of the script before the `exit`:
+
+		po.sh "$0 exited successfully"
+
+That way I don’t need to worry about getting all of the Pushover API details right every time I want to write a shell script which sends a message using Pushover.
+
+Also, `po.sh` will automatically add the `hostname` and `time` to the end of the message, so I’ll never have to wonder “Which computer sent me this message?”
+
+### Before You Can Use This… ###
+
+In order to use this script, you will need to:
+
+*	Sign up for a free account at <https://pushover.net/login> .
+*	Fill out the form at <https://pushover.net/apps/build> to register this script and get your personalized Pushover “Token”.
+
+You will see a form like this:
+
+![](img/https-pushover-net-apps-build.jpg)
+
+You _might_ be able to use `po.sh` for the "Name" in the first field, but I would not be surprised to learn that Pushover requires every application to have a unique name. If so just call it something else.
+
+*Make sure* that you choose "Script" from the “Type” drop down (item #2 in the image). Fill in whatever you want for #3 and #4. You will then need to choose an image to use as an icon, and then check the box to agree to the TOS.
+
+### Once You Have Your Own Pushover Token… ###
+
+Launch the Pushover app on your iPad or iPhone/iPod touch (or Android device) and find the User Key which is specific to that device. (It is *not* the same thing as your *user name* on the Pushover website!)
+
+Then you need to add your Token and User Key somewhere that `po.sh` can find it. There are four options:
+
+1.	Put the two lines inside the `po.sh` file itself. This is the easiest method, assuming that you are not going to post your version of `po.sh` to Github or anywhere that someone might see it, because then they could send you messages and use your API quota.
+2.	Put the two lines inside ~/.zshenv because `po.sh` is a zsh script and therefore it will read ~/.zshenv before it runs.
+3.	Put the two lines inside a plain text file at `"$HOME/Dropbox/etc/posh.txt"`
+4.	Redefine `POSH_FILE=` in `po.sh` to point to a file other than `"$HOME/Dropbox/etc/posh.txt"` and put the two lines inside a plain text file there.
+
+Which two lines? These two:
+
+		PUSHOVER_TOKEN='tttttttttttttt'
+
+		PUSHOVER_USER_KEY='uuuuuuuuuuuu'
+
+Obviously you want to change `tttttttttttttt` to the token you received when you created the “application" on Pushover’s website, and change `uuuuuuuuuuuu` to whatever your User Key actually is, as shown in the settings of the Pushover app on your iOS or Android device. Just make sure that it starts and ends with a straight `'` and that there are no spaces on either side of the `=` and that `PUSHOVER_TOKEN` and `PUSHOVER_USER_KEY` are both uppercase with `_` in the right places, as shown.
+
+*(Sorry to be so specific and step-by-step, but I’m trying to imagine someone who is new at this and isn’t sure what to do. On the other hand, most people reading code on Github probably know how to do this.)*
+
---
+++ po.sh
@@ -0,0 +1,141 @@
+#!/bin/zsh
+# Purpose: send a message via  https://pushover.net
+#
+# 		USING THIS SCRIPT WILL REQUIRE YOU TO REGISTER WITH https://pushover.net/apps
+#		IN ORDER TO GET A "PUSHOVER_TOKEN" WHICH WILL HAVE TO BE ENTERED BELOW.
+#		As of 2014-03-10, each "app" can send 7,500 messages per month for free.
+#
+# From:	Tj Luo.ma
+# Mail:	luomat at gmail dot com
+# Web: 	http://RhymesWithDiploma.com
+# Date:	2014-02-22
+
+
+
+	# PUSHOVER_TOKEN and PUSHOVER_USER_KEY are defined like this:
+	#
+	# 	PUSHOVER_TOKEN='tttttttttttttt'
+	# 	PUSHOVER_USER_KEY='uuuuuuuuuuuu'
+	#
+	# except, of course, replace the "tttttttttttttt" with your actual token and replace
+	# "uuuuuuuuuuuu" with your Pushover user key
+	# (NOTE: THIS IS __NOT__ YOUR PUSHOVER.NET USERNAME).
+	# You can find the Pushover User Key in the settings part of the Pushover app
+	#
+	# You can define them in ~/.zshenv if you want, or in another file
+	#
+	# NOTE:
+	# The format of that file is important.
+	# PUSHOVER_USER_KEY and PUSHOVER_TOKEN must be uppercase
+	# followed by a literal ='
+	# then put in your specific values.
+	# Be sure that there is another literal ' at the end of the line
+	# No spaces!
+	#		PUSHOVER_TOKEN='xxxxx'
+POSH_FILE="$HOME/Dropbox/etc/posh.txt"
+
+	# NOTE! Alternatively, you could define them here.
+	# Just remove the '#' from the start of the next two lines and enter the correct values
+# PUSHOVER_TOKEN='tttttttttttttt'
+# PUSHOVER_USER_KEY='uuuuuuuuuuuu'
+
+
+
+
+
+
+NAME="$0:t:r"
+
+HOST=$(hostname)
+HOST="$HOST:l"
+
+zmodload zsh/datetime
+
+TIME=$(strftime "%Y-%m-%d @ %H.%M.%S" "$EPOCHSECONDS")
+
+## LOGGING SECTION: Start
+		# This is where OS X stores logs which are specific to a user
+	LOG="$HOME/Library/Logs/posh ${TIME}.log"
+
+		# this shouldn't happen, but it might if we try to run this script
+		# on another Unix machine other than OS X
+	[[ ! -d "$LOG:h" ]] && mkdir -p "$LOG:h"
+
+		# rather than have to repeat this syntax every time we want to send
+		# a message to the log file, we'll just define a nice 'log' function.
+	function log {
+					echo "$NAME [$TIME]: $@" | tee -a "$LOG"
+	}
+## LOGGING SECTION: End
+
+	# If the POSH_FILE exists as a readable file, source it.
+	# !! If it does not exist, then PUSHOVER_TOKEN and PUSHOVER_USER_KEY must be defined in ~/.zshenv
+if [ -f "POSH_FILE" -a -r "POSH_FILE" ]
+then
+		source "${POSH_FILE}"
+fi
+
+	# if the variable PUSHOVER_TOKEN is empty, we can't continue
+if [ "$PUSHOVER_TOKEN" = "" ]
+then
+		log "[fatal] [reason] PUSHOVER_TOKEN is empty or undefined."
+		log "[fatal] [explanation] PUSHOVER_USER_KEY and PUSHOVER_TOKEN must be defined in ~/.zshenv or $POSH_FILE or $0"
+		log "[fatal] [information] See $0 for more details. This is a fatal error, meaning that $NAME cannot continue."
+		exit 1
+fi
+
+	# if the variable PUSHOVER_USER_KEY is empty, we can't continue
+if [ "$PUSHOVER_USER_KEY" =  "" ]
+then
+		log "[fatal] [reason]: PUSHOVER_USER_KEY is empty or undefined."
+		log "[fatal] [explanation]: PUSHOVER_USER_KEY and PUSHOVER_TOKEN must be defined in ~/.zshenv or $POSH_FILE or $0"
+		log "[fatal] [information]: See $0 for more details. This is a fatal error, meaning that $NAME cannot continue."
+		exit 1
+fi
+
+	# If there are no arguments given, we can't try to send any message
+if [ "$#" = "0" ]
+then
+		log "[fatal] [reason]: No input received"
+		log "[fatal] [explanation]: $0 is meant to send a message, but you did not give me anything to send."
+		log "[fatal] [information]: This is a fatal error, meaning that $NAME cannot continue."
+		exit 1
+fi
+
+	# first we take whatever the user gave us as input ($@)
+	# and then we append information as to which computer this came from, and at what time
+MESSAGE=`echo "$@ [from $HOST at $TIME]"`
+
+	# use a newline instead of a space as IFS
+IFS=$'\n'
+
+	# THIS IS WHERE WE ACTUALLY DO SOMETHING
+	# using `curl` we attempt to send the message via the Pushover API
+	# and the token and user information we have been given
+	# to send the message that the user specified
+curl --dump-header - \
+	--silent \
+	--location \
+	--form "token=${PUSHOVER_TOKEN}" \
+	--form "user=${PUSHOVER_USER_KEY}" \
+	--form "message=${MESSAGE}" \
+		'https://api.pushover.net/1/messages.json' 2>&1 >> "${LOG}"
+
+EXIT="$?"
+
+if [[ "$EXIT" == "0" ]]
+then
+		exit 0
+
+else
+		log "Exit code for curl was not zero, it was: $EXIT. A log file can be found at $LOG"
+
+		exit 1
+fi
+
+
+
+
+exit
+#
+#EOF
